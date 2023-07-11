df3c6cf0e23dbd30fb5cd98d61f649579723c525
BuildTools <unconfigured@null.spigotmc.org>
2023-07-11 18:39:35 -0400

    Initial commit

---
+++ src/main/java/com/github/compx44/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package com.github.compx44.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
+
